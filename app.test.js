4e886fbf9ba4409f4dbaf5565ba8fb8c854c5326
jayamto <sajaljayanto007@gmail.com>
2023-04-07 23:36:37 +0600

    fix: ut added

---
+++ app.test.js
@@ -0,0 +1,29 @@
+jest.setTimeout(300000);
+const request = require("supertest");
+const app = require("./app");
+const fs = require('fs');
+const config = require('./config')
+
+describe('/GET -> /files/:publicKey', () => {
+  test("It should response the GET method", async () => {
+    const response = await request(app)
+      .get(`/files/${config.publicKey}`);
+    expect(response.statusCode).toBe(200);
+  });
+
+  test("It should response the POST method", async () => {
+    const filePath = `${__dirname}/test-upload/diu.jpeg`;
+    const file = fs.readFileSync(filePath);
+    const response = await request(app)
+      .post("/files")
+      .attach('file', file);
+    expect(response.statusCode).toBe(201);
+  });
+
+  test("It should response the DELETE method", async () => {
+    const response = await request(app)
+      .delete(`/files/${config.privateKey}`)
+    expect(response.statusCode).toBe(200);
+  });
+
+});
